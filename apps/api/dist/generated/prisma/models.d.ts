b35b50517a8386e17a1eb0fca215aad6121600fe
root <root@vmi2857093.contaboserver.net>
2026-06-30 08:47:43 +0200

    Ignore build output directories

--- apps/api/dist/generated/prisma/models.d.ts
+++
@@ -1,6 +0,0 @@
-export type * from './models/School';
-export type * from './models/PendingStudent';
-export type * from './models/Student';
-export type * from './models/User';
-export type * from './models/RefreshToken';
-export type * from './commonInputTypes';
--- apps/api/dist/src/common/permissions/user-permissions.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserPermission, UserRole } from '@generated/prisma/client';
-export { UserPermission };
-export declare const ALL_USER_PERMISSIONS: UserPermission[];
-export declare function hasPermission(role: UserRole, permissions: UserPermission[] | undefined, required: UserPermission): boolean;
-export declare function hasAnyPermission(role: UserRole, permissions: UserPermission[] | undefined, required: UserPermission[]): boolean;
